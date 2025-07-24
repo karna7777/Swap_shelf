3e957ea2e864a64d1966bdc52d763e60ee659cc9
Supreet <102615360+karna7777@users.noreply.github.com>
2025-07-24 18:41:00 +0530

    Moved project to root and fixed casing issues

--- src/Components/popular/Popular.jsx
+++ src/Components/popular/Popular.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-
+import React from 'react'
+import "./Popular.css"
+import Item from "../Item/Item"
+import data_product from '../../assets/data'
 const Popular = () => {
   return (
-    <div>
-      {/* Popular products will be displayed here */}
-      <h2>Popular Products</h2>
+    <div className='popular'>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr/>
+      <div className='popular-item'>
+        {data_product.map((item,i)=>{
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+        })}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Popular;
+export default Popular
